f4e8d163689d73590be5dce1874297f4efec04a5
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-11-29 11:39:59 -0600

    add simple hello world and method to print hello world

---
+++ hello_world.rb
@@ -0,0 +1,11 @@
+# simplest version of hello world
+# run `ruby hello_world.rb` in terminal to see
+puts ("Hello World!")
+
+
+# define 'hi' method, when called puts "Hello Worldd!"
+def hi
+    puts "Hello Worldd!"
+end
+
+print hi()
